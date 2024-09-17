27b47bedf3f153054fbe90a32977544adfe79987
Edgar Raúl Bañuelos Díaz <edgar.banuelos@axity.com>
2024-09-17 08:32:15 -0600

    Creacion de Listas

---
+++ EstructuraDatosAgoDic2024/UsoListas/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+List<int> listaNumeros = new List<int>();
+string respuesta = string.Empty;
+do
+{
+    Console.WriteLine("Ingresa un numero entero:");
+    int numero = Convert.ToInt32(Console.ReadLine());
+    listaNumeros.Add(numero);
+
+    Console.WriteLine("Quieres ingresar otro numero?");
+    respuesta = Console.ReadLine();
+
+} while (respuesta.ToUpper() == "SI");
+
+foreach (int numero in listaNumeros)
+{
+    Console.WriteLine($"{numero}");
+}
+
+Console.WriteLine("Fin del programa");
+Console.ReadKey();
+
+
+
+
