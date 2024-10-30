0379a5e7381138c538dac97906addddccfa1b0d9
Edgar Raúl Bañuelos Díaz <edgar.banuelos@axity.com>
2024-10-30 08:26:39 -0600

    Arbol binario

---
+++ EstructuraDatosAgoDic2024/Arbol/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arbol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Método manual
+            Nodo raiz = new Nodo();
+            raiz.Valor = 3;
+            
+            raiz.NodoDer = new Nodo();
+            raiz.NodoDer.Valor = 4;
+
+            raiz.NodoIzq = new Nodo();
+            raiz.NodoIzq.Valor = 2;
+
+            raiz.NodoIzq.NodoIzq = new Nodo();
+            raiz.NodoIzq.NodoIzq.Valor = 1;
+
+
+            //Método automático
+            int[] vector1 = { 15, 10, 20 };
+            Nodo arbol1 = Nodo.ConstruirArbol(vector1);
+
+            int[] vector2 = { 50, 30, 70, 20, 40, 60, 80, 10, 35, 45 };
+            Nodo arbol3 = Nodo.ConstruirArbol(vector1);
+        }
+    }
+}
---
+++ EstructuraDatosAgoDic2024/ArbolBinario/Nodo.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbolBinario
+{
+    public class Nodo
+    {
+        public int Valor { get; set; }
+
+        public Nodo NodoIzq { get; set; }
+
+        public Nodo NodoDer { get; set; }
+
+        // Constructor para inicializar un nodo con un valor
+        public Nodo(int valor)
+        {
+            Valor = valor;
+            NodoIzq = null;
+            NodoDer = null;
+        }
+
+        public Nodo()
+        {
+            
+        }
+
+        // Método recursivo para insertar un valor en el árbol
+        public void Insertar(int valor)
+        {
+            if (valor < Valor) // Si el valor es menor, va a la izquierda
+            {
+                if (NodoIzq == null)
+                    NodoIzq = new Nodo(valor);
+                else
+                    NodoIzq.Insertar(valor);
+            }
+            else // Si el valor es mayor o igual, va a la derecha
+            {
+                if (NodoDer == null)
+                    NodoDer = new Nodo(valor);
+                else
+                    NodoDer.Insertar(valor);
+            }
+        }
+
+        // Método para construir el árbol a partir de un vector de enteros
+        public static Nodo ConstruirArbol(int[] valores)
+        {
+            if (valores == null || valores.Length == 0)
+                return null;
+
+            Nodo raiz = new Nodo(valores[0]); // La raíz es el primer valor del arreglo
+            for (int i = 1; i < valores.Length; i++)
+            {
+                raiz.Insertar(valores[i]);
+            }
+            return raiz;
+        }
+    }
+}
+}
